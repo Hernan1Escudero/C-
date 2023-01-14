7a9edf631a6c8ec00bfc79670f98ab5b346eb06e
Hernan1Escudero <hernanescudero2@gmail.com>
2023-01-14 15:37:41 -0300

    "Agrego clases"

--- Class1.cs
+++ Class1.cs
@@ -6,30 +6,53 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
-    public class Producto
+    public class Cita
         
     {  // La clase esta basada en un sistema de citas para un centro de estetica
         public DateTime FechaHora;
         public string Lugar;
         public string Cliente;
-        public float Precio;
+        public float Precio;// no me decido si crear una clase de precios aparte
         public string Servicio;// Si buscamos un servicio generalmente es para una zna en especifico, pero hay algunos que tienen varias zonas a elegir por lo que un argumento zonas  sirve para definir cuales
         public string Zona;
 
-        public Producto( ) {
+        public Cita( ) {
 
-           
         
         }
 
-        public Producto(DateTime fechaHora, string lugar, string cliente, float precio, string servicio, string zona)
+        public Cita (DateTime fechaHora, string lugar, string cliente, float precio, string servicio, string zona)
         {
-            FechaHora = fechaHora;
+            this.FechaHora = fechaHora;
             this.Lugar = lugar;
             this.Cliente = cliente;
-            this.Precio = precio;
-            this.Servicio = servicio;
-            this.Zona = zona;
+
         }
     }
+
+    public class Zonas {
+
+        public bool Piernas;
+        public bool Brazos;
+        public bool Abdomen;
+        public bool Espalda;
+        public bool Nuca;
+        public bool AreaDeBikini;
+
+
+
+    }
+
+    public class Servicios {
+        public bool Peeling;
+        public bool Depilacion;
+        public bool Lipo;
+        public bool Crio;
+        public bool Facial;
+        public bool Botox;
+
+
+    }
+
+   
 }
