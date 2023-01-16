8d94d4b5f615c6ed3fb67d6d4b08dfadf8ce6476
Hernan1Escudero <hernanescudero2@gmail.com>
2023-01-16 18:54:26 -0300

    .

--- Program.cs
+++ Program.cs
@@ -4,10 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Cita producto1 = new Cita();
-            producto1.Lugar = "centro";
-            producto1.Zona = "piernas";
-            Console.WriteLine(producto1.Lugar +" "+ producto1.Zona) ;
+            
         }
     }
 
